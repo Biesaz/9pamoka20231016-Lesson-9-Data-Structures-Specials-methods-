21426b40f614acad136fb48f4db08795aabb960d
Vygantas Vilius <biesaz@gmail.com>
2023-10-18 22:06:34 +0300

    "bababa"
    123

--- main.py
+++ main.py
@@ -4,6 +4,18 @@
 # In this lecture we will review some additional functionalities of python built-in data structures. And also review math library
 
 # # List comprehensions
+
+# Every list comprehension in Python includes three elements:
+#            new_list = [expression for member in iterable] -> [išraiška for narys  in kartoti]
+#           new_list = [expression for member in iterable (if conditional)]
+#           new_list = [expression (if conditional) for member in iterable]
+
+# expression      is the member itself, a call to a method, or any other valid expression that returns a value. 
+# In the example above, the expression i * i is the square of the member value.
+# member          is the object or value in the list or iterable. In the example above, the member value is i.
+# iterable        is a list, set, sequence, generator, or any other object that can return its elements one at a time. 
+# In the example above, the iterable is range(10).
+
 # It is a very useful way of making your loops more compact/ readable/ understandable.
 
 # Example:
@@ -44,8 +56,8 @@
 # print([name for name in names if name.startswith("A")])
 # You can even chain more logic with and and or clauses as well:
 
-names = ["Albert", "Alma", "Joseph", "Zoro"]
-print([name for name in names if name.startswith("A") or "e" in name])
+# names = ["Albert", "Alma", "Joseph", "Zoro"]
+# print([name for name in names if name.startswith("A") or "e" in name])
 # As you can see you can chain as much logic as you want to here. But it is best to keep there list comprehensions as short as possible because if you have too much logic here, it might become impossible to understand even for you after couple of days...
 
 # Note ❗ The list, set, tuple comprehensions work exactly the same except that outer brackets are different, everything else stays the same.
